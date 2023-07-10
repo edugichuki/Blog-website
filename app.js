9b909e25b17ba959e8adb3bf5c80f6d56c344245
edugichuki <muriithigichuki854@gmail.com>
2023-07-10 09:02:02 +0300

    Update repo

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.get("/posts/:postId", (req, res) => {
   });
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(5000, function () {
+  console.log("Server started on port 5000");
 });
 
